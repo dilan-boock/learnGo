a931f20e92ff81d69ded0ccfa11e09ed76c5c3d3
Dilan Zenn <u.corporeiscen@gmail.com>
2025-01-14 16:46:00 +0300

    ЦЫЫЫЫЫФРЫЫЫ

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	//С клавиатуры вводится целое число. Определить, из каких цифр оно состоит, то есть вывести на экран отдельные цифры числа.
@@ -9,7 +12,26 @@ func main() {
 	fmt.Println("Введите число: ")
 	fmt.Scan(&chislo)
 
-	ein := chislo % 1e3   // используем деление по модулю, чтобы «считать» с конца
-	zwain := chislo / 1e3 // отрезаем после третьей позиции
-	fmt.Println("Число: ", chislo)
+	// 12345
+	str := strconv.Itoa(chislo)
+	//m := []byte(str)
+
+	//c := str[len(str)-1]
+	m := make([]byte, len(str))
+	copy(m, str)
+	//for i := 0; i < len(str); i++ {
+	//	m[i] = str[len(str)-i-1]
+	//}
+	fmt.Println(string(m))
+	//[len(str)-1] = str[0]
+	//str[0] = c
+
+	//fmt.Println(str[0])
+
+	for chislo != 0 {
+		num := chislo % 10
+		fmt.Println(num)
+		chislo /= 10
+		//fmt.Println(chislo)
+	}
 }
