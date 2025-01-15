d8ba9dc8b9e637c60602d53f1788c4b6901d7713
Dilan Zenn <u.corporeiscen@gmail.com>
2025-01-15 14:00:43 +0300

    Изменение значений в массиве

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import "fmt"
 
-//Напишите программу, которая считает количество элементов (пар ключ-значение) в карте.
+//Реализуйте функцию, которая принимает карту с названиями продуктов и их ценами, а также скидку в процентах. Функция должна обновлять карту, уменьшая все цены на заданный процент.
 
 func main() {
 
-	var massiv = map[string]int{
-		"One":   1,
-		"Two":   2,
-		"Three": 3,
-		"Four":  4,
-		"Five":  5,
-		"Ein":   4}
-	n := 0
+	var massiv = map[string]float32{
+		"Milk":  100,
+		"Cooke": 2540,
+		"Cofie": 321,
+		"Tea":   405,
+		"Fish":  51}
+	var n float32
 
-	for i := 0; i < len(massiv); i++ {
-		n++
+	for i := range massiv {
+		n = massiv[i]
+		massiv[i] = n - (n/100)*10
 	}
-	fmt.Println("В массиве ", n, "пар ключ-значение")
+	fmt.Println("Новый список цен: ", massiv)
 }
