b943cb05e38af433db7a90f8842f4e76e46c911f
Dilan Zenn <u.corporeiscen@gmail.com>
2025-01-15 15:13:45 +0300

    Пороговое значение карты - с отдельной функцией

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 //Реализуйте функцию, которая принимает карту и пороговое значение. Возвращайте новую карту, содержащую только те элементы, где значение больше порога.
 
+func returnMap(massiv map[string]float32, n float32) map[string]float32 {
+	slice := map[string]float32{}
+
+	for i := range massiv {
+		for massiv[i] > n {
+			slice[i] = massiv[i]
+			break
+		}
+	}
+	return slice
+}
+
 func main() {
 
 	var massiv = map[string]float32{
@@ -13,14 +25,7 @@ func main() {
 		"Tea":   40,
 		"Fish":  51}
 	fmt.Println("Старый массив: ", massiv)
-
-	slice := map[string]float32{}
-
-	for i := range massiv {
-		for massiv[i] > 30 {
-			slice[i] = massiv[i]
-			break
-		}
-	}
+	n := float32(30)
+	slice := returnMap(massiv, n)
 	fmt.Println("Массив после порогового значения: ", slice)
 }
