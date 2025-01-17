2f9fb5d063c7b6006696f3263e70837c1511ed55
Dilan Zenn <u.corporeiscen@gmail.com>
2025-01-17 14:55:09 +0300

    Объединение в один файл и разбивка по функциям

--- main.go
+++ main.go
@@ -2,30 +2,91 @@ package main
 
 import "fmt"
 
-//Реализуйте функцию, которая принимает карту и пороговое значение. Возвращайте новую карту, содержащую только те элементы, где значение больше порога.
+//Напишите функцию, которая создаёт "обратную" карту. Например, из карты вида {"a": 1, "b": 2, "c": 2} сделайте карту вида {1: ["a"], 2: ["b", "c"]}
 
-func returnMap(massiv map[string]float32, n float32) map[string]float32 {
-	slice := map[string]float32{}
+func returnMap(massiv map[string]float32) map[float32]string {
+	var n float32
+	var s string
+	slice := map[float32]string{}
 
 	for i := range massiv {
-		for massiv[i] > n {
-			slice[i] = massiv[i]
-			break
-		}
+		n = massiv[i]
+		s = i
+		slice[n] = s
 	}
 	return slice
+}
+
+func valueMap(chislo int) (int, int) {
+	even := 0
+	uneven := 0
+
+	for chislo != 0 {
+		num := chislo % 10
+		if num%2 == 0 {
+			even++
+		}
+		if num%2 != 0 {
+			uneven++
+		}
+		fmt.Println(num)
+		chislo /= 10
+		//fmt.Println(chislo)
+	}
+	return even, uneven
+}
+
+func modifyMap(massiv map[string]float32) map[string]float32 {
+	var n float32
+
+	for i := 0; i < len(massiv); i++ {
+		n++
+		for i := range massiv {
+			n = massiv[i]
+			massiv[i] = n - (n/100)*10
+		}
+	}
+	return massiv
+}
+
+func countMap(massiv map[string]int) int {
+	n := 0
+
+	for i := 0; i < len(massiv); i++ {
+		n++
+	}
+	return n
 }
 
 func main() {
 
 	var massiv = map[string]float32{
-		"Milk":  10,
-		"Cooke": 20,
-		"Cofie": 30,
-		"Tea":   40,
+		"Milk":  100,
+		"Cooke": 2540,
+		"Cofie": 321,
+		"Tea":   405,
 		"Fish":  51}
 	fmt.Println("Старый массив: ", massiv)
-	n := float32(30)
-	slice := returnMap(massiv, n)
-	fmt.Println("Массив после порогового значения: ", slice)
+	slice := returnMap(massiv)
+	fmt.Println("Новый массив: ", slice)
+
+	var chislo int
+	fmt.Println("Введите число: ")
+	fmt.Scan(&chislo)
+	even, uneven := valueMap(chislo)
+	fmt.Println("Сумма четных чисел: ", even)
+	fmt.Println("Сумма нечетных чисел: ", uneven) //Подсчет колва
+
+	var massiv2 = map[string]int{
+		"One":   1,
+		"Two":   2,
+		"Three": 3,
+		"Four":  4,
+		"Five":  5,
+		"Ein":   4}
+	n := countMap(massiv2)
+	fmt.Println("В массиве ", n, "пар ключ-значение") //ключ-значение
+
+	massivRes := modifyMap(massiv)
+	fmt.Println("Новый список цен: ", massivRes)
 }
